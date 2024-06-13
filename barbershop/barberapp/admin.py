bc7313e43327c0b3fea0a2a190e6f7689e69a319
Jesse Lee Ross <jesseross001@gmail.com>
2024-06-13 18:46:52 +0000

    start of custom admin panel

--- barbershop/barberapp/admin.py
+++ barbershop/barberapp/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
-from .models import Service, Staff, Booking, BusinessHours, GalleryImage
+from .models import Service, Staff, BusinessHours, Booking, GalleryImage
+
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+
+class BusinessHoursAdmin(admin.ModelAdmin):
+    list_display = ('staff', 'day_of_week', 'open_time', 'close_time')
+    list_filter = ('day_of_week', 'staff')
+    ordering = ('day_of_week',)
 
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ['client_name', 'service', 'staff', 'date_time', 'client_contact']
-    list_filter = ['date_time', 'service', 'staff']
+    list_display = ('client_name', 'service', 'staff', 'date_time', 'client_contact')
+    list_filter = ('date_time', 'service', 'staff')
+    search_fields = ['client_name', 'client_contact']
 
 admin.site.register(Service)
-admin.site.register(Staff)
+admin.site.register(Staff, StaffAdmin)
+admin.site.register(BusinessHours, BusinessHoursAdmin)
 admin.site.register(Booking, BookingAdmin)
-admin.site.register(BusinessHours)
 admin.site.register(GalleryImage)
