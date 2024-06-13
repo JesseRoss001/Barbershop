838b37f7b509f1266729e042e77ccece599f2026
Jesse Lee Ross <jesseross001@gmail.com>
2024-06-13 14:54:39 +0000

    Carousel in place

--- barbershop/barberapp/urls.py
+++ barbershop/barberapp/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path ,include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('booking/', views.booking, name='booking'),
     path('gallery/', views.gallery, name='gallery'),
+    path('about/', views.about, name='about'),  # New URL for the about page
+    path('blog/', views.blog, name='blog'),     # New URL for the blog page
 ]
-
-
--- barbershop/barberapp/views.py
+++ barbershop/barberapp/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from .models import Service, Booking, GalleryImage
 
 def home(request):
     return render(request, 'barberapp/home.html')
 
 def booking(request):
     services = Service.objects.all()
-    return render(request, 'booking.html', {'services': services})
+    return render(request, 'barberapp/booking.html', {'services': services})
 
 def gallery(request):
     images = GalleryImage.objects.all()
-    return render(request, 'gallery.html', {'images': images})
+    return render(request, 'barberapp/gallery.html', {'images': images})
 
+def about(request):
+    return render(request, 'barberapp/about.html')  # Render an about page template
+
+def blog(request):
+    return render(request, 'barberapp/blog.html')  # Render a blog page template
