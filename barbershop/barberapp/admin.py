06b229ff320b57904d46c140ddbfcc45baa13eaf
Jesse Lee Ross <jesseross001@gmail.com>
2024-06-14 00:10:32 +0000

    Update the times for staff

--- barbershop/barberapp/admin.py
+++ barbershop/barberapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Service, Staff, BusinessHours, Booking, GalleryImage
 
 
@@ -12,8 +13,24 @@ class BusinessHoursInline(admin.TabularInline):
     max_num = 7  # Limits to exactly seven days
 
 class StaffAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email')
+    list_display = ('name', 'email', 'weekly_hours_display')
+
     inlines = [BusinessHoursInline]
+
+    def weekly_hours_display(self, obj):
+        hours = BusinessHours.objects.filter(staff=obj)
+        display_html = ""
+        for hour in hours:
+            display_html += f"<div><strong>{hour.get_day_of_week_display()}:</strong> "
+            if hour.is_open:
+                display_html += f"{hour.open_time.strftime('%H:%M')} - {hour.close_time.strftime('%H:%M')} "
+                display_html += f"(Lunch: {hour.lunch_start.strftime('%H:%M')} - {hour.lunch_end.strftime('%H:%M')})"
+            else:
+                display_html += "Closed"
+            display_html += "</div>"
+        return format_html(display_html)
+
+    weekly_hours_display.short_description = "Weekly Hours"
 
 
 
